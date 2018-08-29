c2732a4d4dcaf5a1ec4fe676e545120a9759453d
Ricardo Lira <ricardo.19.rl09@gmail.com>
2018-08-28 21:12:24 -0500

    Variable de entorno en MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -14,7 +14,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:abc123@ds137102.mlab.com:37102/cafe';
+    urlDB = process.env.MONGO_URL;
 
 }
 process.env.URLDB = urlDB;
